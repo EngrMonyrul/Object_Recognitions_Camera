a3cc9dcda7dd5b0c18a016379d0d3b6e3a6ae383
EngrMonyrul <121379927+EngrMonyrul@users.noreply.github.com>
2023-09-17 15:23:22 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+import 'camera_screen_view.dart';
+
+late List<CameraDescription> cameras;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    print('Error: $e.code\nError Message: $e.message');
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomePage(cameras),
+    );
+  }
+}
